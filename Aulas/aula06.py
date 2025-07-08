012b95fcf8f0399e3d47a66a38d01cbd1a2bf9c6
ItalloMene <italloborgessilva22@gmail.com>
2025-07-08 18:11:34 -0300

    testes aula 06

--- Aulas/aula06.py
+++
@@ -1,6 +0,0 @@
-# AULA DE TIPOS PRIMITIVOS
-
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite mais um número: '))
-s = n1 + n2
-print('A soma vale {}'.format(s))
---
+++ Aulas/aula06a.py
@@ -0,0 +1,15 @@
+# AULA DE TIPOS PRIMITIVOS
+
+n1 = input('Digite um número: ')
+print(type(n1))
+n2 = input('Digite outro número: ')
+print(type(n2))
+s = n1 + n2
+print('A soma vale {}'.format(s))
+
+n1 = int(input('Digite um número: '))
+print(type(n1))
+n2 = int(input('Digite mais um número: '))
+print(type(n2))
+s = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
