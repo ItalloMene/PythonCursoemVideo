7bc03f698d706effc4c1dc79d0068e67db0e5785
ItalloMene <italloborgessilva22@gmail.com>
2025-08-05 18:22:34 -0300

    códigos estudados na aula 09

---
+++ Aulas/aula09.py
@@ -0,0 +1,35 @@
+print('''Welcome! Are you completely new to programing?
+about why and how to get started with Python. Fortunately
+an experienced programer in any programming language
+(whatever it may be) can pick uo Python very quickly.
+Its also easy for beginner to use and learn, so jump in!''')
+print(' ')
+
+frase = '  Curso em Vídeo Python  '
+print(frase)
+print(frase[3])
+print(frase[3:13])
+print(frase[:13])
+print(frase[13:])
+print(frase[1:15])
+print(frase[1:15:2])
+print(frase[1::2])
+print(frase[::2])
+print(frase.count('o'))
+print(frase.count('O'))
+print(frase.upper().count('O'))
+print(len(frase))
+print(len(frase.strip()))
+print(frase.replace('Python', 'Andorid'))
+print(frase)
+frase = frase.replace('Python', 'Android')
+print(frase)
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.find('Vídeo'))
+print(frase.find('vídeo'))
+print(frase.lower().find('vídeo'))
+dividido = frase.split()
+print(dividido)
+print(dividido[0])
+print(dividido[2][3])
