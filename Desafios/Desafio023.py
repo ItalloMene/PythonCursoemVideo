0637acbc6f74fed3ab45f7f6558d6b07c2917919
ItalloMene <italloborgessilva22@gmail.com>
2025-08-08 20:07:45 -0300

    Desafio 023 atualizado

--- Desafios/Desafio023.py
+++ Desafios/Desafio023.py
@@ -4,6 +4,6 @@ mostre cada um dos digitos separados.
 Ex: Digite um número: 1834
 unidade:4 dezena:3 centena:8 milhar:1
 '''
-num = str(input('Digite um número de 0000 a 9999 (sem espaços): '))
+num = input('Digite um número de 0 a 9999 (sem espaços): ').zfill(4)
 print('| Unidade: {} | Dezena: {} | Centena: {} | Milhar: {} |'.format(num[3], num[2], num[1], num[0]))
 
