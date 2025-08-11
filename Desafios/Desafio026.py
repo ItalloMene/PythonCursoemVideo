37d28e08a6960ba9365f11d5203307987d93a62f
ItalloMene <italloborgessilva22@gmail.com>
2025-08-11 15:18:02 -0300

    ex026 feito - Correção Desafio026

--- Desafios/Desafio026.py
+++ Desafios/Desafio026.py
@@ -1,9 +1,9 @@
-'''
+"""
 DESAFIO 026 - Faça um programa que leia uma frase pelo teclado e mostre:
 - Quantas vezes aparece a letra "A";
 - Em que posição ela aparece a primeira vez;
-- Em que posição ela aparece pela ultima vez;
-'''
+- Em que posição ela aparece pela última vez;
+"""
 frase = str(input('Por favor, digite uma frase: '))
 aparece = frase.upper().count('A')
 primeiro = frase.upper().find('A')
