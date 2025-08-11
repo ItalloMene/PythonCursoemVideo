dc71c67d6ccb21d2dfe270dfe63630129a589daa
ItalloMene <italloborgessilva22@gmail.com>
2025-08-11 13:17:32 -0300

    ajustes recomendados no ex023

--- Exercícios/ex023.py
+++ Exercícios/ex023.py
@@ -1,9 +1,9 @@
-'''
+"""
 DESAFIO 023 - Faça um programa que leia um número de 0 a 9999 e
 mostre cada um dos digitos separados.
 Ex: Digite um número: 1834
-unidade:4 dezena:3 centena:8 milhar:1
-'''
+unidade: 4 dezena: 3 centena: 8 milhar: 1
+"""
 
 '''
 # RESPOSTA 01: Método por String
