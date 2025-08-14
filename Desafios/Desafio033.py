fce52aa4626f26986ca1b50c9ef164fb844d7df5
ItalloMene <italloborgessilva22@gmail.com>
2025-08-14 13:36:47 -0300

    Desafio033 feito

---
+++ Desafios/Desafio033.py
@@ -0,0 +1,31 @@
+'''
+DESAFIO 033 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor
+'''
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+print(' ')
+
+print('Analisando os números: {}, {}, {}.'.format(n1, n2, n3))
+if n1 > n2 and n1 > n3:
+  print('O primeiro número ({}) é o maior número entre os demais.'.format(n1))
+else:
+    if n2 > n1 and n2 > n3:
+        print('O segundo número ({}) é o maior número entre os demais.'.format(n2))
+    else:
+        if n3 > n1 and n3 > n2:
+            print('O terceiro número ({}) é o maior número entre os demais'.format(n3))
+
+
+if n1 < n2 and n1 < n3:
+    print('O primeiro número ({}) é o menor número entre os demais.'.format(n1))
+else:
+    if n2 < n1 and n2 < n3:
+        print('O segundo número ({}) é o menor número entre os demais.'.format(n2))
+    else:
+        if n3 < n1 and n3 < n2:
+            print('O terceiro número ({}) é o menor número entre os demais.'.format(n3))
+
+if n1 == n2 and n1 == n3 and n2 == n3:
+    print('Números iguais encontrados, não há maior ou menor entre eles.')
