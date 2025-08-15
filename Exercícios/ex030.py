114d602f82140e1c424cbc6af69ce5d318523958
ItalloMene <italloborgessilva22@gmail.com>
2025-08-15 17:04:50 -0300

    ex030 feito - Correção Desafio030

---
+++ Exercícios/ex030.py
@@ -0,0 +1,9 @@
+"""
+DESAFIO 030 - Crie um programa que leia um número inteiro e mostre se ele é PAR ou IMPAR.
+"""
+número = int(input('Me diga um número qualquer: '))
+resultado = número % 2
+if resultado == 0:
+    print('O número {} é PAR!'.format(número))
+else:
+    print('O número {} é IMPAR!'.format(número))
