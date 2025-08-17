68d7c3f03a4fe77009821c7d5dcff260ac2bae9b
ItalloMene <italloborgessilva22@gmail.com>
2025-08-17 12:35:53 -0300

    ex034 feito - Correção Desafio034

---
+++ Exercícios/ex034.py
@@ -0,0 +1,12 @@
+"""
+DESAFIO 034 - Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+Para salários superiores a R$1.250,00 calcule um aumento de 10%.
+Para inferiores ou iguais o aumento é de 15%.
+"""
+
+salário = float(input('Qual é o salário do funcionário? R$'))
+if salário <= 1250:
+    novo = salário + (salário * 15 / 100)
+else:
+    novo = salário + (salário * 10 / 100)
+print('Quem ganhava R${:.2f} passa ganhar R${:.2f} agora'.format(salário, novo))
