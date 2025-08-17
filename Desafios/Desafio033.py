b7f8cf6c2eee58186dddaaabab2504593469cc55
ItalloMene <italloborgessilva22@gmail.com>
2025-08-17 12:15:18 -0300

    ex033 feito - Correção Desafio033

--- Desafios/Desafio033.py
+++ Desafios/Desafio033.py
@@ -1,6 +1,6 @@
-'''
+"""
 DESAFIO 033 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor
-'''
+"""
 
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
