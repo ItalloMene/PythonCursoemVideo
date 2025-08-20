ddcb28b0b4ffd5e043e42f4427d309bf2c7efc0f
ItalloMene <italloborgessilva22@gmail.com>
2025-08-20 15:23:20 -0300

    adição de cores no desafio 003

--- Desafios/Desafio003.py
+++ Desafios/Desafio003.py
@@ -3,4 +3,4 @@
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
 s = n1 + n2
-print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+print('A soma entre {}{}{} e {}{}{} vale {}{}{}'.format('\033[0;32m', n1 ,'\033[m','\033[0;32m',n2,'\033[m','\033[34m',s,'\033[m'))
