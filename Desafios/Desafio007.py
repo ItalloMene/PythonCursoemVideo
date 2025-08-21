1fbab99be5a0e69fc001df03813b51e247ffa341
ItalloMene <italloborgessilva22@gmail.com>
2025-08-21 18:45:08 -0300

    Adição de cores no Desafio007

--- Desafios/Desafio007.py
+++ Desafios/Desafio007.py
@@ -4,4 +4,4 @@ aluno = input('Por favor digite o nome do aluno: ')
 nota1 = float(input('Digite a primeira nota de {}: '.format(aluno)))
 nota2 = float(input('Digite a segunda nota de {}: '.format(aluno)))
 media = (nota1 + nota2) / 2
-print('A média das notas de {} é {:.2f}'.format(aluno, media))
+print('A \033[1;4;33m Média \033[m das notas de \033[1;4;36m {} \033[m é \033[1;4;33m {:.2f} '.format(aluno, media))
