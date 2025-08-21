27f3ad348bcc3132e3f5de3f5e617ac3222099cf
ItalloMene <italloborgessilva22@gmail.com>
2025-08-21 18:19:31 -0300

    Adição de cores no Desafio005

--- Desafios/Desafio005.py
+++ Desafios/Desafio005.py
@@ -1,5 +1,11 @@
 # DESAFIO 005 - Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+cores = {
+    'Negrito-azul' : '\033[1;4;44m',
+    'Negrito-vermelho' : '\033[1;4;41m',
+    'Negrito-verde' : '\033[1;4;42m',
+    'Limpar' : '\033[m'
+}
 numero = int(input('`Por favor, Digite um número: '))
 antes = numero - 1
 depois = numero + 1
-print('O número digitado foi {}, seu sucessor é {} e seu antecessor é {}. '.format(numero, depois, antes))
+print('O número digitado foi {}{}{}, seu sucessor é {}{}{} e seu antecessor é {}{}{}. '.format(cores['Negrito-azul'],numero, cores['Limpar'],cores['Negrito-verde'], depois, cores['Limpar'], cores['Negrito-vermelho'],antes, cores['Limpar']))
