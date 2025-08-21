239a518a54b149c841baefc3be660473bf85e567
ItalloMene <italloborgessilva22@gmail.com>
2025-08-21 18:37:53 -0300

    adição de cores no Desafio006

--- Desafios/Desafio006.py
+++ Desafios/Desafio006.py
@@ -3,4 +3,4 @@ numero = float(input('Por favor, digite um número: '))
 dobro = numero * 2
 triplo = numero * 3
 raiz = numero ** (1/2)
-print('O número digitado é {:.2f}, seu dobro é {:.2f}, seu triplo é {:.2f} e sua raiz é {:.2f}'.format(numero,dobro,triplo,raiz))
+print('O número digitado é \033[1;4;45m{:.2f}\033[m, seu dobro é \033[1;4;42m{:.2f}\033[m, seu triplo é \033[1;4;41m{:.2f}\033[m e sua raiz é \033[1;4;44m{:.2f}\033[m'.format(numero,dobro,triplo,raiz))
