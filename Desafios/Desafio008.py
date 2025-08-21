40c7cbe59a2c0e9b40e88984c60ee76b9f5d87c0
ItalloMene <italloborgessilva22@gmail.com>
2025-08-21 18:54:52 -0300

    Cores adicionadas ao Desafio008

--- Desafios/Desafio008.py
+++ Desafios/Desafio008.py
@@ -3,4 +3,4 @@
 metros = float(input('Por favor, digite um valor em metros (x.xx): '))
 centimetros =  metros * 100
 milimetros = metros * 1000
-print('O valor digitado foi {}m, sendo {}cm (centímetros) e {}mm (milímetros). '.format(metros, centimetros, milimetros))
+print('\033[1;4;41m O valor digitado foi: \033[1;4;42m {}m \033[1;41m, sendo \033[1;4;46m {}cm \033[1;41m (centímetros)  e \033[1;4;43m {}mm \033[1;41m (milímetros).\033[m'.format(metros, centimetros, milimetros))
