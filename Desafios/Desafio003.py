36df2e49e196985987df4db235c06c414eee0faf
ItalloMene <italloborgessilva22@gmail.com>
2025-08-21 10:35:52 -0300

    novos ajustes de cores no Desafio003

--- Desafios/Desafio003.py
+++ Desafios/Desafio003.py
@@ -1,6 +1,10 @@
 # DESAFIO 3 - Crie um programa que leia dois números e mostre a soma entre elas.
-
+cores = {
+'Bold-Verde' : '\033[32m',
+'Bold-Azul' : '\033[34m',
+'Limpar' : '\033[m'
+}
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
 s = n1 + n2
-print('A soma entre {}{}{} e {}{}{} vale {}{}{}'.format('\033[0;32m', n1 ,'\033[m','\033[0;32m',n2,'\033[m','\033[34m',s,'\033[m'))
+print('A soma entre {}{}{} e {}{}{} vale {}{}{}.'.format( cores['Bold-Verde'], n1, cores['Limpar'], cores['Bold-Verde'], n2, cores['Limpar'], cores['Bold-Azul'], s, cores['Limpar']))
