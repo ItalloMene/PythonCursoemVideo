c3a460b34b0764fd40c14bff8391f38df83a8d8c
ItalloMene <italloborgessilva22@gmail.com>
2025-08-29 13:51:45 -0300

    Adição de cores Desafio010

--- Desafios/Desafio010.py
+++ Desafios/Desafio010.py
@@ -1,9 +1,19 @@
 # DESAFIO 010 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
-# Considere US$1,00 = R$ 3,27
-# Valores atuais: Dólar = 5.59 Euro = 6.52 Iene = 0,038 (Julho, 2025)
+# Considere US$1,00 = R$3,27
+# Valores atuais: Dólar = 5,59 Euro = 6,52 Iene = 0,038 (julho, 2025)
+
+cores = {
+    'Real' : '\033[1;32m',
+    'Dollar' : '\033[1;37m',
+    'Euro' : '\033[1;34m',
+    'Ienes' : '\033[1;31m',
+    'Limpar' : '\033[m'
+}
+
+
 
 real = float(input('Por favor, digite qual valor em Real você possui na carteira (xx.xx): R$'))
-dolar = real / 5.59
+dollar = real / 5.59
 euro = real / 6.52
 iene = real / 0.038
-print('Com {:.2f} Reais você pode comprar {:.2f} Dolares, ou {:.2f} Euros, ou {:.2f} Ienes '.format(real, dolar, euro, iene))
+print('Com {}{:.2f} Reais{} você pode comprar {}{:.2f} Dollars{}, ou {}{:.2f} Euros{}, ou {}{:.2f} Ienes{}'.format(cores['Real'], real, cores['Limpar'], cores['Dollar'], dollar, cores['Limpar'], cores['Euro'], euro, cores['Limpar'], cores['Ienes'], iene, cores['Limpar']))
