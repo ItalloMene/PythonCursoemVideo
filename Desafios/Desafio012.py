10e9fb4524265e34e38c0486888dd4a9e619ff1d
ItalloMene <italloborgessilva22@gmail.com>
2025-08-29 14:32:47 -0300

    adição de cores ao Desafio012

--- Desafios/Desafio012.py
+++ Desafios/Desafio012.py
@@ -1,6 +1,6 @@
-# DESAFIO 012 - Faça um algoritimo que leia a o preço de um produto e mostre seu novo preço com 5% de desconto.
+# DESAFIO 012 - Faça um algoritimo que leia o preço de um produto e mostre o seu novo preço com 5% de desconto.
 preco = float(input('Por favor digite o preço atual do produto: R$'))
 desconto = preco * (5/100)
 novopreco = preco - desconto
-print('Com o desconto de 5% o novo preço do produto é R${:.2f}.'.format(novopreco))
+print('Com o desconto de \033[1;92m5%\033[m o novo preço do produto é: \033[1;4;92mR${:.2f}\033[m.'.format(novopreco))
 
