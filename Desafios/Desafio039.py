4683fb806a6b4137819aa7ff71b56f1e2b9a24da
ItalloMene <italloborgessilva22@gmail.com>
2025-09-16 13:48:08 -0300

    Adição de escolha de Gênero no Desafio 039

--- Desafios/Desafio039.py
+++ Desafios/Desafio039.py
@@ -14,15 +14,23 @@ print('+' * 25)
 nascimento = int(input('Digite seu ano de nascimento: '))
 ano_atual = datetime.now().year
 idade = ano_atual - nascimento
-
+print('''Selecione uma das opções de gênero
+[ 1 ] Feminino
+[ 2 ] Masculino''')
+gen = int(input('Digite o número correspondente a sua escolha: '))
 print(' ')
 print('        \033[4;93mRESPOSTA\033[m')
-if idade < 18:
-    print('Você ainda irá se alistar.')
-    print('Tendo \033[4;93m{}\033[m anos de idade ainda faltam \033[4;92m{}\033[m anos até você precisar se alistar.'.format(idade, 18 - idade))
-elif idade > 18:
-    print('Você já passou do tempo do alistamento.')
-    print('Tendo \033[4;93m{}\033[m anos de idade você já passou \033[91;4m{}\033[m anos do alistamento.'.format(idade, idade - 18))
+if gen == 1:
+    print('Por ser do Gênero Feminino você não precisa se alistar.')
+elif gen == 2:
+    if idade < 18:
+        print('Você ainda irá se alistar.')
+        print('Tendo \033[4;93m{}\033[m anos de idade ainda faltam \033[4;92m{}\033[m anos até você precisar se alistar.'.format(idade, 18 - idade))
+    elif idade > 18:
+        print('Você já passou do tempo do alistamento.')
+        print('Tendo \033[4;93m{}\033[m anos de idade você já passou \033[91;4m{}\033[m anos do alistamento.'.format(idade, idade - 18))
+    else:
+        print('Você já está no tempo do alistamento.')
+        print('Tendo \033[4;93m{}\033[m anos você está na idade de se alistar.'.format(idade))
 else:
-    print('Você já está no tempo do alistamento.')
-    print('Tendo \033[4;93m{}\033[m anos você está na idade de se alistar.'.format(idade))
+    print('\033[91m[ERRO] Opção de Gênero não identificada.')
