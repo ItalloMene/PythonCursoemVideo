ae5534b91217e11c9e85edaeb2787a523c9ef27d
ItalloMene <italloborgessilva22@gmail.com>
2025-09-16 13:37:43 -0300

    ex039 feito - Correção Desafio039

---
+++ Exercícios/ex039.py
@@ -0,0 +1,46 @@
+"""
+Desafio 039: Faça um programa que leia o ano de nascimento de um jovem e informe, conforme a sua idade:
+- Se ele ainda vai se alistar no serviço militar;
+- Se é a hora de se alistar;
+- Se já passou do tempo do alistamento
+O seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+"""
+
+# RESPOSTA 01: ELIF
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento.'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {}'.format(ano))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você já deveria ter se alistado há {} anos.'.format(saldo))
+    ano = atual - saldo
+    print('Seu alistamento foi em {}.'.format(ano))
+
+# RESPOSTA 02: ELSE
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento.'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {}'.format(ano))
+else:
+    saldo = idade - 18
+    print('Você já deveria ter se alistado há {} anos.'.format(saldo))
+    ano = atual - saldo
+    print('Seu alistamento foi em {}.'.format(ano))
+    
