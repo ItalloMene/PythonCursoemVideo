9abc5bed974254f169de88d2289410a840216f36
ItalloMene <italloborgessilva22@gmail.com>
2025-09-22 17:51:57 -0300

    Desafio047 - feito

---
+++ Desafios/Desafio047.py
@@ -0,0 +1,14 @@
+"""
+Desafio 047: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+"""
+from time import sleep
+print('=' * 30)
+print('{}{:^30}{}'.format('\033[1;94m','CONTADOR DE PARES', '\033[m'))
+print('=' * 30)
+for cont in range(1,51):
+    if cont % 2 == 0:
+        print('{}{:^30}{}'.format('\033[96m', cont, '\033[m'))
+        sleep(0.5)
+print('=' * 30)
+print('{}{:^30}{}'.format('\033[1;94m', 'FIM', '\033[m'))
+print('=' * 30)
