bef1cef56f2282c68eea069e78b7f245d2916c5b
ItalloMene <italloborgessilva22@gmail.com>
2025-09-22 17:31:43 -0300

    Desafio046

---
+++ Desafios/Desafio046.py
@@ -0,0 +1,16 @@
+"""
+Desafio 046: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos
+de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+"""
+
+from time import sleep
+from emoji import emojize
+print('=' * 30)
+print('{:^30}'.format('CONTAGEM FOGOS DE ARTIFÍCIO'))
+print('=' * 30)
+for c in range(10, 0, -1):
+    print('{:^30}'.format(c))
+    sleep(1)
+print('=' * 30)
+print('{:^4}{:^20}{:^4}'.format(emojize(':fireworks:'), 'ESTOURO DE FOGOS!', emojize(':fireworks:')))
+print('=' * 30)
