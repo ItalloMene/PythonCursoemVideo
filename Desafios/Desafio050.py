8dcbd7e2006e6284baeb2b7df6990cf9fc4dffb0
ItalloMene <italloborgessilva22@gmail.com>
2025-09-30 12:26:26 -0300

    ex050 feito - Correção Desafio050

--- Desafios/Desafio050.py
+++ Desafios/Desafio050.py
@@ -6,11 +6,13 @@ print('=' * 45)
 print('{}{:^45}{}'.format('\033[96m', 'SOMA DOS PARES', '\033[m'))
 print('=' * 45)
 soma = 0
+cont = 0
 for contador in range(1,7):
     numero = int(input('  {}) Por favor digite um número inteiro: '.format(contador)))
     if numero % 2 == 0:
         soma = soma + numero
+        cont += 1
 print('=' * 45)
 print('{}{:^45}{}'.format('\033[96m', 'RESULTADO', '\033[m'))
-print('    A soma entre os valores pares é: {}{}{}'.format('\033[32m', soma, '\033[m'))
+print('{:^8}Você digitou {}{}{} valores PARES. \n{:^8}E a  soma entre eles é: {}{}{}.'.format(' ','\033[32m', cont, '\033[m', ' ','\033[32m', soma, '\033[m'))
 print('=' * 45)
