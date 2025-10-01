ba96dbaa8f846051a018300ce839bfbb19aa481e
ItalloMene <italloborgessilva22@gmail.com>
2025-10-01 18:35:06 -0300

    ex052 feito - Correção Desafio052

--- Desafios/Desafio052.py
+++ Desafios/Desafio052.py
@@ -1,7 +1,6 @@
 """
 Desafio 052: Faça um programa que leia um número inteiro e diga se ele é ou não um número primo
 """
-from operator import truediv
 
 print('=+' * 15)
 print('{:^30}'.format('DESCOBRINDO NÚMERO PRIMO'))
