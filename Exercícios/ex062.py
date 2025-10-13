6f66f52030ab98e59cd084d2e428a18929a1555f
ItalloMene <italloborgessilva22@gmail.com>
2025-10-13 15:55:02 -0300

    ex062 feito - Correção Desafio062

---
+++ Exercícios/ex062.py
@@ -0,0 +1,22 @@
+"""
+Desafio 062 - Melhore o Desafio061, perguntando para o usuário se ele quer mostrar mais alguns termos.
+O programa encerra quando ele disser que quer mostrar 0 termos.
+"""
+print(' Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razão =  int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+# Uso de laços aninhados
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} → '.format(termo), end='')
+        termo += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
