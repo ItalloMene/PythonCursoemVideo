bcd83ba54bb01665d3fbdbfcf954d92a17d04fcc
ItalloMene <italloborgessilva22@gmail.com>
2025-10-13 15:17:14 -0300

    ex060 feito - Correção Desafio060

---
+++ Exercícios/ex060.py
@@ -0,0 +1,23 @@
+"""
+Desafio 060: Faça um programa que leia um número qualquer e mostre o seu fatorial.
+Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
+"""
+"""
+# RESPOSTA 01: utilizando import factorial do math
+from math import factorial
+n = int(input('Digite um número para calcular seu Fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}.'.format(n, f))
+"""
+
+# RESPOSTA 02: utilizando while
+n = int(input('Digite um número para calcular seu Fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c # mesmo que f = f * c
+    c -= 1
+print('{}.'.format( f))
