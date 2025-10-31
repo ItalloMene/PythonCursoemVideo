439bda59c67de1c9c412c41d6823c493f8647538
ItalloMene <italloborgessilva22@gmail.com>
2025-10-31 09:44:06 -0300

    Desafio078 feito

--- Desafios/Desafio078.py
+++ Desafios/Desafio078.py
@@ -3,21 +3,33 @@ Desafio 078: Faça um programa que leia 5 valores numéricos e guarde os numa li
 No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 """
 numeros = list()
+
+print('=' * 50)
 for contador in range(1, 6):
     numero = int(input(f'→ Digite o {contador}º número para lista: '))
     numeros.append(numero)
-print('-' * 45)
-for contador in range(0, len(numeros)):
-    if contador == 0:
-        maior = menor = numeros[0]
-        posiçãomaior = posiçãomenor = numeros.index(numeros[0])
-    else:
-        if maior >= numeros[contador]:
-            maior = numeros[contador]
-            posiçãomaior = numeros.index(numeros[contador])
-        if menor <= numeros[contador]:
-            menor = numeros[contador]
-            posiçãomenor = numeros.index(numeros[contador])
+
+print('-' * 50)
 print(f'→ Lista formada: {numeros}')
-print(f'→ Maior número encontrado: {maior} | Na posição: {posiçãomaior + 1}')
-print(f'→ Menor número encontrado: {menor} | Na posição: {posiçãomenor + 1}')
+
+maior = menor = numeros[0]
+
+for contador in range(0, len(numeros)):
+    if numeros[contador] > maior:
+        maior = numeros[contador]
+    if numeros[contador] < menor:
+        menor = numeros[contador]
+
+posicoes_maior = list()
+posicoes_menor = list()
+
+for posicao, valor in enumerate(numeros):
+    if valor == maior:
+        posicoes_maior.append(posicao + 1)
+    if valor == menor:
+        posicoes_menor.append(posicao + 1)
+
+print(f'→ Maior número encontrado: {maior} | Nas posições: {posicoes_maior}')
+print(f'→ Menor número encontrado: {menor} | Nas posições: {posicoes_menor}')
+print('=' * 50)
+
