fba66bd622d2ba5c06a7c4d0e9e58b548141c5ab
ItalloMene <italloborgessilva22@gmail.com>
2025-12-01 11:09:50 -0300

    ex097 Feito - Correção Desafio097

---
+++ Exercícios/ex097.py
@@ -0,0 +1,20 @@
+"""
+Desafio 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro
+e mostre uma mensagem com tamanho adaptável.
+Ex: escreva('Olá, Mundo!')
+saída:
+~~~~~~~~~~~~
+Olá, Mundo!
+~~~~~~~~~~~~
+"""
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# Programa principal
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no Youtube')
+escreva('Cev')
