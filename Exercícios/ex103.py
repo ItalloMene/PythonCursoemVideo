9f9f5833ac2b37e28dce6d168c7d5bbae4ab9bae
ItalloMene <italloborgessilva22@gmail.com>
2025-12-13 12:31:10 -0300

    ex103 Feito - Cooreção Desafio103

---
+++ Exercícios/ex103.py
@@ -0,0 +1,20 @@
+"""
+Desafio 103: Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais:
+o nome do jogador e quantos gols ele marcou.
+O programa devera ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+"""
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gols no campeonato.')
+
+
+# Programa principal
+n = str(input('Nome do Jogador: '))
+g = str(input('Número de Gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n,g)
