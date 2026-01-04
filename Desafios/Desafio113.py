dc417f62b9129a027eb7b713b4c57d406e4beca7
ItalloMene <italloborgessilva22@gmail.com>
2026-01-04 16:14:05 -0300

    Ajustes finais Desafio113

--- Desafios/Desafio113.py
+++ Desafios/Desafio113.py
@@ -19,35 +19,36 @@ def leiaFloat(msg):
 
 # Programa Principal
 linha()
-print(f'\033[93m{"INTEIRO E REAL":^50}\033[m')
+print(f'\033[94m{"INTEIRO E REAL":^50}\033[m')
 linha()
-
+i = r = 0
 while True:
-    ok = False
-
+    ok1 = False
     try:
         i = leiaInt('→ Digite um número inteiro: ')
     except Exception as erro:
         print(f'\033[31mERRO: por favor, digite um número inteiro válido!\033[m')
-    else:
-        ok = True
-
-    if ok:
+    except KeyboardInterrupt:
+        print('\n\033[93m usuário preferiu não informar o valor.\033[m')
         break
-
-while True:
-    ok = False
-
-    try:
-        r = leiaFloat('→ Digite um número real: ')
-    except Exception as erro:
-        print(f'\033[31mERRO: por favor, digite um número real válido!\033[m')
     else:
-        linha()
-        print(f'→ O valor inteiro digitado foi \033[96m{i}\033[m e o real foi \033[96m{r:.1f}\033[m')
-        print('→ Fim do programa! Volte sempre!')
-        ok = True
-
-    if ok:
+        while True:
+            ok2 = False
+            try:
+                r = leiaFloat('→ Digite um número real: ')
+            except Exception as erro:
+                print(f'\033[31mERRO: por favor, digite um número real válido!\033[m')
+            except KeyboardInterrupt:
+                print(f'\n\033[93mO usuário preferiu não informar o valor.\033[m')
+                break
+            else:
+                ok2 = True
+            if ok2:
+                break
+        ok1 = True
+    if ok1:
         break
+linha()
+print(f'→ O valor inteiro digitado foi \033[96m{i}\033[m e o real foi \033[96m{r:.1f}\033[m')
+print('→ Fim do programa! Volte sempre!')
 linha()
